e43a515b70a04992e6343473229ea7d4de04f377
rezendevm <rezendevm1@gmail.com>
2022-03-04 14:32:09 -0300

    proceeding setup

---
+++ aprendendoPython/basics.py
@@ -0,0 +1,8 @@
+from unicodedata import name
+
+
+print ("assigning variables")
+name = input("Your name")
+age = input("Your age")
+
+print ("My name is " + name + " and I am " + str(age) + " years old")
